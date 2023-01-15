f2f97bfbf38cb75205c769c43519e8712f84491d
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-15 13:00:04 +0000

    .

--- Pre-Examen/x08a-arrayInit-proc-for_loop-in/src/example.adb
+++ Pre-Examen/x08a-arrayInit-proc-for_loop-in/src/example.adb
@@ -3,13 +3,12 @@ package body Example with SPARK_Mode is
    procedure Init_For_Loop (V : out T_Vector; Value : Integer) is
    begin
       V := (others => 0);
-
-      for J in V'Range loop
-         V (J) := Value;
-
---         pragma Loop_Invariant
---           (for all K in V'First .. J =>
---              V (K) = Value);
+      for i in V'Range loop
+         V(i) := Value;
+         pragma Loop_Invariant(for all J in V'First .. i =>
+                                 V(J) = Value);
+         pragma Loop_Invariant(for all K in i+1 .. V'Last =>
+                              V(K) = 0);
       end loop;
    end Init_For_Loop;
 
--- Pre-Examen/x08a-arrayInit-proc-for_loop-in/src/example.ads
+++ Pre-Examen/x08a-arrayInit-proc-for_loop-in/src/example.ads
@@ -1,14 +1,15 @@
 package Example with SPARK_Mode is
 
-   type T_Vector is array (Positive range 1 .. 3) of Integer;
+   type T_Vector is array (Positive range 1 .. Positive'Last) of Integer;
    --  Level 0: 'Last = 20
 
    procedure Init_For_Loop (V : out T_Vector; Value : Integer) with
-     Global  => null,
-     Depends => (V => Value),
-     Pre     => V'Length > 0,
-     Post    => (for all J in V'Range =>
-                   V (J) = Value);
+     Global => null,
+     Depends => (V =>  Value),
+     Pre => (Value > 0) and then (V'Last <= Positive'Last -1),
+     Post => (for all J in V'Range =>
+                V(J) = Value);
+
 
 end Example;
 
